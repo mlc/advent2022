f2f18e7a6cc96b428a98441055faf3764924b1ef
mike castleman <m@mlcastle.net>
2022-11-30 21:15:59 -0500

    init

---
+++ util.ts
@@ -0,0 +1,101 @@
+// deno-lint-ignore-file no-explicit-any
+
+export const identity = <T>(x: T): T => x;
+
+export function sumBy(array: ArrayLike<number>): number;
+
+export function sumBy<T>(
+  array: ArrayLike<T>,
+  validator: (elt: T, index: number) => number | boolean
+): number;
+
+export function sumBy<T>(
+  array: ArrayLike<T>,
+  validator: (elt: any, index: number) => number | boolean = identity
+): number {
+  return Array.prototype.reduce.call(
+    array,
+    (accum: unknown, element: T, index: number) =>
+      (accum as number) + (validator(element, index) as number),
+    0
+  ) as number;
+}
+
+export function productBy(array: readonly number[]): number;
+
+export function productBy<T>(
+  array: readonly T[],
+  validator: (elt: T, index: number) => number
+): number;
+
+export function productBy<T>(
+  array: readonly T[],
+  validator: (elt: any, index: number) => number = identity
+): number {
+  return array.reduce(
+    (accum, element, index) => accum * validator(element, index),
+    1
+  );
+}
+
+export const readSplit = (day: number, separator = '\n', t = false): string[] =>
+  Deno.readTextFileSync(`input${day < 10 ? '0' : ''}${day}${t ? 't' : ''}`)
+    .trim()
+    .split(separator);
+
+export const setIntersect = <T>(
+  set: Set<T> = new Set(),
+  ...sets: Set<T>[]
+): Set<T> => {
+  if (!sets || sets.length === 0) {
+    return set;
+  }
+
+  return new Set([...set].filter((it) => sets.every((s) => s.has(it))));
+};
+
+export const mapChars = (
+  str: string,
+  op: (ch: string, index: number) => string
+) => {
+  const arr = new Array(str.length);
+  for (let i = 0; i < str.length; ++i) {
+    arr[i] = op(str[i], i);
+  }
+  return arr.join('');
+};
+
+export const addVectors = (
+  vector: readonly number[] = [],
+  ...vectors: readonly number[][]
+): number[] => vector.map((x, i) => vectors.reduce((a, v) => a + v[i], x));
+
+export const range = (start: number, end: number, step = 1) => {
+  const result = [];
+  for (let i = start; i < end; i += step) {
+    result.push(i);
+  }
+  return result;
+};
+
+export type Coord = [number, number];
+
+export const neighbors = (
+  [x, y]: Coord,
+  includeDiagonals = false,
+  includeSelf = false
+): Coord[] => {
+  const result: Coord[] = [
+    [x - 1, y],
+    [x + 1, y],
+    [x, y - 1],
+    [x, y + 1],
+  ];
+  if (includeDiagonals) {
+    result.push([x - 1, y - 1], [x - 1, y + 1], [x + 1, y - 1], [x + 1, y + 1]);
+  }
+  if (includeSelf) {
+    result.push([x, y]);
+  }
+  return result;
+};
