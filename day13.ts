917a69769dcd793f90312dae8e44d412168c75c8
mike castleman <m@mlcastle.net>
2022-12-13 19:46:32 -0500

    cleanup

--- day13.ts
+++ day13.ts
@@ -1,49 +1,38 @@
-import { readSplit, show, sumBy } from './util.ts';
+import { Pair, productBy, readSplit, show, sumBy } from './util.ts';
 
 type Packet = number | Packet[];
 
 const parse = (x: string) =>
-  x.split('\n').map((row) => JSON.parse(row)) as [Packet, Packet];
+  x.split('\n').map<Packet>((row) => JSON.parse(row)) as Pair<Packet>;
 
 const input = (await readSplit(13, '\n\n', false)).map(parse);
 
-const compare = (
-  left: Packet | undefined,
-  right: Packet | undefined
-): number => {
+const compare = (left: Readonly<Packet>, right: Readonly<Packet>): number => {
   if (typeof left === 'number') {
     if (typeof right === 'number') {
       return left - right;
-    } else if (Array.isArray(right)) {
-      return compare([left], right);
     } else {
-      return 1000;
+      return compare([left], right);
     }
-  } else if (Array.isArray(left)) {
+  } else {
     if (typeof right === 'number') {
       return compare(left, [right]);
-    } else if (Array.isArray(right)) {
-      for (let i = 0; i < Math.max(left.length, right.length); ++i) {
+    } else {
+      for (let i = 0; i < Math.min(left.length, right.length); ++i) {
         const trial = compare(left[i], right[i]);
         if (trial) {
           return trial;
         }
       }
-      return 0;
-    } else {
-      return 1000;
+      return left.length - right.length;
     }
-  } else {
-    return -compare(right, left);
   }
 };
 
 await show(sumBy(input, ([l, r], idx) => (compare(l, r) > 0 ? 0 : idx + 1)));
 
-const copy = [...input.flat(), [[2]], [[6]]]
-  .sort(compare)
-  .map((elt) => JSON.stringify(elt));
-const idx = copy.indexOf('[[2]]');
-const idx2 = copy.indexOf('[[6]]');
+const terminals: Packet[] = [[[2]], [[6]]];
 
-await show((idx + 1) * (idx2 + 1));
+const copy = [...input.flat(), ...terminals].sort(compare);
+
+await show(productBy(terminals, (t) => copy.indexOf(t) + 1));
